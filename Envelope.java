df7bf6017dc8b5c6dd2d2209c160b3ab224b82a6
Alex Valentin <128508828+AMVGH@users.noreply.github.com>
2024-01-03 12:15:11 -0500

    Update Envelope.java

--- Envelope.java
+++ Envelope.java
@@ -1,11 +1,9 @@
 abstract public class Envelope extends Mail {
     private double thickness;
     Envelope(){
-        //Calls parent default constructor by default when instantiated?
         thickness = 0;
     }
     Envelope(String deliveryAddressPass, String returnAddressPass, double widthPass, double lengthPass, double thicknessPass){
-        //If passing to parent constructor, super must be first statement in constructor body
         super(deliveryAddressPass, returnAddressPass, widthPass, lengthPass);
         thickness = thicknessPass;
     }
